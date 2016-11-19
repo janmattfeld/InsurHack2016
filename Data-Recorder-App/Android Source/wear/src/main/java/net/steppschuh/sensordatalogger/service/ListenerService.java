0bbb68195d7d827b6e56b765cdb62b1c171ebdd8
Robert Stark <robert.stark@nexenio.com>
2016-11-19 11:33:48 +0100

    data-recorder-app

---
+++ Data-Recorder-App/Android Source/wear/src/main/java/net/steppschuh/sensordatalogger/service/ListenerService.java
@@ -0,0 +1,15 @@
+package net.steppschuh.sensordatalogger.service;
+
+import android.util.Log;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class ListenerService extends WearableListenerService {
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.i("test", "onMessageReceived()");
+    }
+
+}
